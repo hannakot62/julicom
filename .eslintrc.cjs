c79d3e3f7cbcc9fcf3cb8ca754adcf613aec0450
HANNAKOT62 <2kotyagova6@gmail.com>
2023-12-01 00:46:48 +0300

    chore: added formatter, updated linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "eslint-config-prettier"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -12,7 +16,35 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {allowConstantExport: true},
     ],
+    "no-unused-vars": [
+      "error",
+      {
+        "vars": "all",
+        "args": "after-used",
+        "ignoreRestSiblings": true,
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "react/react-in-jsx-scope": "off"
+  },
+  settings: {
+    "react": {
+      "version": "detect"
+    },
+    "import/resolver": {
+      "node": {
+        "paths": [
+          "src"
+        ],
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <div>
-hello
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                hello
+            </div>
+        </>
+    )
 }
+
 
 export default App
