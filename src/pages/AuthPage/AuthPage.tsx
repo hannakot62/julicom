181d5effa4a9dfc302e13aa189b65c51de2ce1a9
HANNAKOT62 <2kotyagova6@gmail.com>
2023-12-05 17:50:22 +0300

    feat: finished auth page

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -3,20 +3,32 @@ import Input from "../../UI/Input/Input";
 import ResetButton from "../../UI/ResetButton/ResetButton";
 import SaveButton from "../../UI/SaveButton/SaveButton";
 import {useSelector} from "react-redux";
-import {Navigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 
 export default function AuthPage() {
-    const user = useSelector(state=>state.user.user)
-    if (user !== null) {
-        <Navigate to="/" />
+    const user = useSelector(state => state.user.user)
+    const navigate = useNavigate()
+    const [isNew, setIsNew] = useState(false);
+
+    const handleIsNew = (e) => {
+        setIsNew(!isNew)
     }
+
+    useEffect(() => {
+        if (user !== null) {
+            navigate("/")
+        }
+    }, [user])
+
     return (
         <div className={style.wrapper}>
             <div className={style.form}>
-                <h1>Войти</h1>
+                <h1>{isNew ? "Зарегистрироваться" : "Войти"}</h1>
                 <div className={style.inputs}>
-                    <Input label={"Введите почту"} placeholder={"hereismy@mail.com"}/>
+                    {isNew && <Input label={"Введите имя пользователя"} placeholder={"username"}/>}
+                    <Input label={"Введите почту"} placeholder={"hereismy@mail.com"} type={"email"}/>
                     <Input label={"Введите пароль"} type={"password"} placeholder={"password"}/>
                 </div>
                 <div className={style.buttons}>
@@ -24,7 +36,10 @@ export default function AuthPage() {
                     <SaveButton text={"Войти"}/>
                 </div>
             </div>
-           <h2> Нет аккаунта? <button>Зарегистрироваться</button></h2>
+            <h2> {isNew ? "" : "Нет аккаунта?"}
+                <button
+                    onClick={(e) => handleIsNew(e)}>{isNew ? "У меня есть учетная запись" : "Зарегистрироваться"}</button>
+            </h2>
         </div>
     )
 }
